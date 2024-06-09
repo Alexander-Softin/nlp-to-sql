c6c2bea7851972d168e3ccd7764811eca4476ca7
Alexander-Softin <81094118+Alexander-Softin@users.noreply.github.com>
2024-06-09 18:56:25 +0300

    Update index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,7 +4,7 @@ import router from './routes';
 import 'dotenv/config'
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.json());
